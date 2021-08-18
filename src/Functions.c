2b804f6566138d09457cc7ba6299e45dfa6483ec
Lassi Köykkä <lassi_koykka@hotmail.com>
2021-08-18 22:51:28 +0300

    Initial commit.

---
+++ src/Functions.c
@@ -0,0 +1,6 @@
+#include "include/Functions.h"
+#include <stdlib.h>
+
+float randomColor() {
+    return (float)(0.1f * (rand() % 11));
+}
---
+++ src/include/Functions.h
@@ -0,0 +1,6 @@
+#if !defined(FUNCTIONS_H_INCLUDED)
+#define FUNCTIONS_H_INCLUDED
+
+float randomColor();
+
+#endif // FUNCTIONS_H_INCLUDED
